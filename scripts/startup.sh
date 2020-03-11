29602ff5f2368bbdf40fa701409ce45214605ae5
diversemix <diversemix@gmail.com>
2020-03-11 08:43:48 +0000

    Fix the cron schedule

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -4,8 +4,8 @@ echo "Building config..."
 . /mail-config/build_ssmtp_conf.sh
 
 echo "Modifying cron..."
-echo "*       8       *       *       *       run-parts /etc/periodic/daily8am" >> /etc/crontabs/root
-echo "*       13       *       *       *       run-parts /etc/periodic/daily1pm" >> /etc/crontabs/root
+echo "0       8       *       *       *       run-parts /etc/periodic/daily8am" >> /etc/crontabs/root
+echo "0       13       *       *       *       run-parts /etc/periodic/daily1pm" >> /etc/crontabs/root
 crontab -l
 
 echo "Starting cron..."
